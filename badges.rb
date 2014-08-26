235d7fb98b4883a1aeb966104dfbb9c1dfffd87d
chenlizhan <chung1350@hotmail.com>
2014-08-26 23:45:45 +0800

    Create the badges.rb to get the information on the codecademy

---
+++ badges.rb
@@ -0,0 +1,24 @@
+require 'nokogiri'				# Import necessary packages
+require 'open-uri'
+
+user_information = Hash.new(0)	# user_information is a hash to store 'title' and 'date' of badges
+count = 0						# Initial the loop counter
+
+puts "Please type the username: "	# Prompt user to type username
+username = gets.chomp
+
+url = "http://www.codecademy.com/users/#{username}/achievements"
+doc = Nokogiri::HTML(open(url))		# Get the HTML source from the url
+
+titles = doc.xpath("//div[@class = 'grid-row']//h5[@class = 'margin-top--1']") 	# Store badges' titles as an array
+dates = doc.xpath("//small[@class = 'text--ellipsis']")							# Store badges' date
+
+while count < titles.length do
+	user_information[titles[count].text] = dates[count].text
+	count += 1
+end
+
+puts "#{username} has collected #{titles.length} badges."
+user_information.each do |title, date|
+	puts "Badges '#{title}' on #{date}."
+end
