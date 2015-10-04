867a76cfcc246210ae8ac97028d2ae90dc796e49
Soumya Ray <soumya.ray@gmail.com>
2015-10-04 09:52:15 +0800

    Streamline tests

--- spec/badges_test.rb
+++ spec/badges_test.rb
@@ -2,24 +2,6 @@ require 'minitest/autorun'
 require 'minitest/rg'
 require './lib/codebadges.rb'
 
-badge = {
-  'Sorting Your Friends' => 'May 18, 2014',
-  '100 Exercises' => 'May 18, 2014',
-  'Max Streak Count of 3' => 'May 11, 2014',
-  'Design a Button for Your Website' => 'May  9, 2014',
-  'CSS: An Overview' => 'May  9, 2014',
-  'Build Your Own Webpage' => 'May  9, 2014',
-  '50 Exercises' => 'May  9, 2014',
-  'Clickable Photo Page' => 'May  9, 2014',
-  'HTML Basics III' => 'May  9, 2014',
-  '25 points earned in one day' => 'May  9, 2014',
-  'HTML Basics' => 'May  9, 2014',
-  '25 Exercises' => 'May  9, 2014',
-  '10 Exercises' => 'Dec 12, 2012',
-  'Max Streak Count of 1' => 'Dec 12, 2012',
-  'First Lesson' => 'Jun 20, 2012'
-}
-
 badges_from_file = {
   'soumya.ray' => {
     'Sorting Your Friends' => 'May 18, 2014',
@@ -99,15 +81,15 @@ badges_from_file = {
   }
 }
 
-badges_found = CodeBadges::CodecademyBadges.get_badges('soumya.ray')
+USERNAME = 'soumya.ray'
+badges_found = CodeBadges::CodecademyBadges.get_badges(USERNAME)
 
 describe 'Get badges for one user' do
-
   it 'has the right number of badges' do
-    badges_found.size.wont_be :<, badge.size
+    badges_found.size.wont_be :<, badges_from_file[USERNAME].size
   end
 
-  badge.map do |b_name, b_date|
+  badges_from_file[USERNAME].map do |b_name, b_date|
     it "finds '#{b_name}' badge" do
       badges_found[b_name].must_equal Date.parse(b_date)
     end
