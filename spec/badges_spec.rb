5cb6ec048ad814d4845cc8e5cd9499431058e8c3
Soumya Ray <soumya.ray@gmail.com>
2015-10-08 10:16:53 +0800

    VCR: Allow recording and playback of HTTP calls during testing
    - gemspec: add vcr and webmock dependencies
    - badges_spec: VCR record full playback of user badges tests
    - fixtures/vcr_cassettes/ : auto created recordings of http traffic

--- spec/badges_spec.rb
+++ spec/badges_spec.rb
@@ -1,20 +1,30 @@
 require 'minitest/autorun'
 require 'minitest/rg'
 require 'yaml'
+require 'vcr'
+require 'webmock/minitest'
 require './lib/codebadges.rb'
 
 USERNAME = 'soumya.ray'
 badges_from_file = YAML.load(File.read('./spec/testfiles/badges.yml'))
-badges_found = CodeBadges::CodecademyBadges.new(USERNAME).badges
 
-describe 'Get badges for one user' do
-  it 'has the right number of badges' do
-    badges_found.size.wont_be :<, badges_from_file[USERNAME].size
-  end
+VCR.configure do |config|
+  config.cassette_library_dir = 'fixtures/vcr_cassettes'
+  config.hook_into :webmock
+end
 
-  badges_from_file[USERNAME].map do |b_name, b_date|
-    it "finds '#{b_name}' badge" do
-      badges_found[b_name].must_equal Date.parse(b_date)
+VCR.use_cassette('badges') do
+  badges_found = CodeBadges::CodecademyBadges.new(USERNAME).badges
+
+  describe 'Get badges for one user' do
+    it 'has the right number of badges' do
+      badges_found.size.wont_be :<, badges_from_file[USERNAME].size
+    end
+
+    badges_from_file[USERNAME].map do |b_name, b_date|
+      it "finds '#{b_name}' badge" do
+        badges_found[b_name].must_equal Date.parse(b_date)
+      end
     end
   end
 end
