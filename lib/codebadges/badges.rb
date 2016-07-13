bfecc1eefa77d113e5aa92956b1201b79a15dc87
Soumya Ray <soumya.ray@gmail.com>
2016-07-13 16:24:29 +0800

    Refactor to allow change of web automation suite

--- lib/codebadges/badges.rb
+++ lib/codebadges/badges.rb
@@ -5,50 +5,47 @@ require 'open-uri'
 require 'date'
 
 module CodeBadges
-  # This class get the user account as an input
-  # return a hash of user's badges information
-  class GetCodecademyBadges
+  ## Changeable Cademy scraper
+  class CademySite
     USERS_URL = 'https://www.codecademy.com/users'.freeze
     ACHIEVEMENTS_DIR = 'achievements'.freeze
+    LOGIN_URL = 'https://www.codecademy.com/login'.freeze
+    USERNAME_TEXTFIELD_ID = 'user_login'.freeze
+    PASSWORD_TEXTFIELD_ID = 'user_password'.freeze
+    LOGIN_BUTTON_ID = 'user_submit'.freeze
+
     ACHIEVEMENT_XPATH = "//div[contains(@class,'achievement-card')]".freeze
     ACH_TITLE_XPATH = 'h5'.freeze
     ACH_DATE_XPATH = 'small/small'.freeze
 
-    def initialize(username, password)
-      @username = username
-      @password = password
-    end
+    attr_accessor :html
 
-    def call(cadet)
-      open_browser
-      @browser.goto('https://www.codecademy.com/login')
-
-      enter_credentials
-      @browser.button(id: 'user_submit').click
-      @browser.goto cadet_achievements_url(cadet)
-
-      extract_achievements.tap { close_browser }
-    end
-
-    private
-
-    def open_browser
+    def initialize
       @headless ||= Headless.new
       @browser ||= Watir::Browser.new(:firefox)
+      return unless block_given?
+
+      yield self
+      close
     end
 
-    def close_browser
+    def self.cadet_achievements_url(cadet)
+      "#{USERS_URL}/#{cadet}/#{ACHIEVEMENTS_DIR}"
+    end
+
+    def goto(url)
+      @browser.goto(url)
+    end
+
+    def close
       @browser.close
       @headless.destroy
     end
 
-    def cadet_achievements_url(cadet)
-      "#{USERS_URL}/#{cadet}/#{ACHIEVEMENTS_DIR}"
-    end
-
-    def enter_credentials
-      @browser.text_field(id: 'user_login').set(@username)
-      @browser.text_field(id: 'user_password').set(@password)
+    def login(username, password)
+      @browser.text_field(id: USERNAME_TEXTFIELD_ID).set(username)
+      @browser.text_field(id: PASSWORD_TEXTFIELD_ID).set(password)
+      @browser.button(id: LOGIN_BUTTON_ID).click
     end
 
     def extract_achievements
@@ -58,6 +55,26 @@ module CodeBadges
         date = Date.parse(achievement.xpath(ACH_DATE_XPATH).text)
         [title, date]
       end.to_h
+    end
+  end
+
+  # Service object class that takes credentials and username as an input
+  # returns a hash of user's badges information
+  class GetCodecademyBadges
+    def initialize(username, password)
+      @username = username
+      @password = password
+    end
+
+    def call(cadet)
+      CademySite.new do |site|
+        site.goto CademySite::LOGIN_URL
+        site.login(@username, @password)
+        site.goto CademySite.cadet_achievements_url(cadet)
+        @badges = site.extract_achievements
+      end
+
+      @badges
     end
   end
 end
